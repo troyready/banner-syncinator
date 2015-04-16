66f14cd43c2d69b05416f6113fcdad70a157baa7
Adam Crownoble <adam.crownoble@biola.edu>
2015-04-16 13:53:40 -0700

    Sync residence groups into trogdir

---
+++ lib/workers/groups/residence.rb
@@ -0,0 +1,44 @@
+module Workers
+  module Groups
+    class Residence
+      include Sidekiq::Worker
+      include Sidetiq::Schedulable
+
+      recurrence do
+        weekly
+      end
+
+      def perform
+        Group.all.each do |group|
+          sql = "SELECT DISTINCT pidm FROM bsv_health_center WHERE bh_street1 = '#{group.street1}' AND bh_street3 = '#{group.street3}'"
+          GroupSynchronizer.new(group.name, sql).call
+        end
+      end
+
+      private
+
+      class Group
+        attr_reader :name, :building, :floor, :wing
+
+        def initialize(name, building, floor=nil, wing=nil)
+          @name = name
+          @building = building
+          @floor = floor
+          @wing = wing
+        end
+
+        alias :street1 :building
+
+        def street3
+          [floor, wing.to_s.titleize].join(',')
+        end
+
+        def self.all
+          @all ||= Settings.residence.groups.map do |group|
+            Group.new(group.name, group.building, group.floor, group.wing)
+          end
+        end
+      end
+    end
+  end
+end
